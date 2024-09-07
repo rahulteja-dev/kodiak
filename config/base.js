1e7ec0f392814ad7a96c40ea51e5b7184ce8b29e
A Rahul Teja <amilineni.teja@lenskart.in>
2024-09-08 03:21:49 +0530

    ESLint Changes

---
+++ config/base.js
@@ -0,0 +1,46 @@
+import eslint from '@eslint/js'
+import importPlugin from 'eslint-plugin-import'
+import tseslint from 'typescript-eslint'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+
+export default tseslint.config(
+    {
+        files: ["**/*.js", "**/*.ts", "**/*.tsx"],
+        plugins: {
+            import: importPlugin
+        },
+        extends: [
+            eslint.configs.recommended,
+            ...tseslint.configs.recommended,
+            ...tseslint.configs.recommendedTypeChecked,
+            ...tseslint.configs.stylisticTypeChecked,
+        ],
+        rules: {
+
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+            ],
+            "@typescript-eslint/consistent-type-imports": [
+                "warn",
+                { prefer: "type-imports", fixStyle: "separate-type-imports" },
+            ],
+            "@typescript-eslint/no-unnecessary-condition": [
+                "error",
+                {
+                    allowConstantLoopConditions: true,
+                },
+            ],
+            "@typescript-eslint/no-misused-promises": [
+                2,
+                { checksVoidReturn: { attributes: false } },
+            ],
+            "@typescript-eslint/no-non-null-assertion": "error",
+            "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+        },
+    },
+    {
+        linterOptions: { reportUnusedDisableDirectives: true },
+        languageOptions: { parserOptions: { project: true } },
+    }
+)
---
+++ config/next.js
@@ -0,0 +1,19 @@
+import nextPlugin from "@next/eslint-plugin-next";
+
+/** @type {Awaited<import('typescript-eslint').Config>} */
+export default [
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        plugins: {
+            "@next/next": nextPlugin,
+        },
+        rules: {
+            ...nextPlugin.configs.recommended.rules,
+            ...nextPlugin.configs["core-web-vitals"].rules,
+            "@next/next/no-duplicate-head": "off",
+            "@next/next/no-html-link-for-pages": "error",
+            "@next/next/no-img-element": "error",
+            "react-hooks/exhaustive-deps": "off",
+        },
+    },
+]
