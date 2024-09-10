55cf05e5fad29d4f24e2c27b289c0c3c75d6197c
A Rahul Teja <amilineni.teja@lenskart.in>
2024-09-10 11:31:16 +0530

    refactor: refactored header and main into multiple components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,12 @@
-import Header from "@/components/header";
+import Header from "@/components/Header";
+import Main from "@/components/Main";
 
 export default function Home() {
 	return (
 		<div className="text-foreground mx-auto min-h-screen max-w-screen-xl px-1 py-12 font-inter md:px-4 md:py-20 lg:px-8 lg:py-0">
 			<div className="lg:flex lg:justify-between lg:gap:4">
 				<Header />
-				<main id="content" className="pt-24 lg:w-1/2 lg:py-20">
-					<section id="about" className="h-96">
-						About Section
-					</section>
-					<section id="services" className="h-96">
-						Services Section
-					</section>
-					<section id="projects" className="h-96">
-						Projects Section
-					</section>
-					<section id="blog" className="h-96">
-						Blogs Section
-					</section>
-					<section id="experience" className="h-96">
-						Expericnce Section
-					</section>
-					<section id="contact" className="h-96">
-						Contact Section
-					</section>
-				</main>
+				<Main />
 			</div>
 		</div>
 	);
---
+++ src/helpers/constants.ts
@@ -0,0 +1,36 @@
+const constants = {
+	WELCOME_TEXT: "Hy! This is",
+	NAME: "Rahul Teja",
+	TITLE: "Senior Frontend Engineer",
+	EXPERIENCE: "9+ years of experience",
+	DESCRIPTION:
+		"I design pixel-perfect, seamlessly smooth user experiences using React, Next.js & other's. I love what I do!!",
+	NAV_ITEMS: [
+		{
+			label: "About",
+			href: "about",
+		},
+		{
+			label: "Services",
+			href: "services",
+		},
+		{
+			label: "Projects",
+			href: "projects",
+		},
+		{
+			label: "Experience",
+			href: "experience",
+		},
+		{
+			label: "Blog",
+			href: "blog",
+		},
+		{
+			label: "Contact",
+			href: "contact",
+		},
+	],
+};
+
+export default constants;
