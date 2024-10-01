90eaea0d04b10f73e74bd632d56ecc33c4ec7464
A Rahul Teja <amilineni.teja@lenskart.in>
2024-10-01 16:00:08 +0530

    fix: skills overflow for mobile

--- src/components/Main/about.tsx
+++ src/components/Main/about.tsx
@@ -14,10 +14,10 @@ import Transition from "../UI/transition";
 const About = () => {
 	return (
 		<div className="container mx-auto px-4">
+			<div>
+				About Me Hello I am rahul teja Amilineni working in lenskart as senior software developer
+			</div>
 			<div className="grid grid-cols-2 md:grid-cols-3 md:grid-rows-[10] gap-4 auto-rows-[minmax(200px,auto)]">
-				{/* <Card className="md:col-span-2 md:row-span-2 group">
-					<div className="text-2xl custom-gradient-text">About Me</div>
-				</Card> */}
 				<Card className="col-span-1 row-span-1 group" width="max-w-sm">
 					<div>
 						<div className="text-2xl custom-gradient-text">{constants.EXPERIENCE}</div>
@@ -27,15 +27,15 @@ const About = () => {
 						<p className="custom-bento-text">{constants.EXPERIENCE_DESCRIPTION}</p>
 					</div>
 				</Card>
-				<Card className="col-span-1 row-span-1 group" width="max-w-sm">
-					<div>
-						<div className="text-2xl custom-gradient-text">{constants.EXPERIENCE}</div>
-						<div className="text-7xl custom-gradient-text">
-							9<span className="text-4xl">+</span>
-						</div>
-						<p className="custom-bento-text">{constants.EXPERIENCE_DESCRIPTION}</p>
-					</div>
+				<Card className="col-span-1 row-span-1 group">
+					<div className="text-2xl custom-gradient-text">Team Player</div>
+					<p className="custom-bento-text text-md mt-4">
+						Worked closely with cross-functional teams to design, develop, and deploy web
+						applications.
+					</p>
 				</Card>
+
+				{/* Contact Me Card */}
 				<Card className="col-span-2 row-span-1 group" width="max-w-md">
 					<div className="text-2xl custom-gradient-text">
 						{constants.CONTACT_ME} <span className="text-4xl">👋🏻</span>{" "}
@@ -47,57 +47,51 @@ const About = () => {
 						<Button label={constants.CONTACT_ME_BUTTON} />
 					</a>
 				</Card>
-
+				<Card className="col-span-1 row-span-1 group">
+					<div className="text-2xl custom-gradient-text">Snag Solver</div>
+					<p className="custom-bento-text text-md mt-4">
+						Expert in identifying complex UI/UX issues and optimizing functionality across multiple
+						devices.
+					</p>
+				</Card>
+				<Card className="col-span-1 row-span-2 md:col-span-3 md:row-span-1 group" width="max-w-xl">
+					<div className="text-2xl custom-gradient-text">Skills</div>
+					<div className="mt-5 flex flex-wrap gap-8">
+						<Transition duration={500} scale={100}>
+							<HTMLLogo width={50} height={50} />
+						</Transition>
+						<Transition duration={500} scale={100}>
+							<CSSLogo width={50} height={50} />
+						</Transition>
+						<Transition duration={500} scale={125}>
+							<JavaScriptLogo width={50} height={50} />
+						</Transition>
+						<Transition duration={500} scale={125}>
+							<TypeScriptLogo width={50} height={50} />
+						</Transition>
+						<Transition duration={500} scale={125}>
+							<ReactLogo width={50} height={50} />
+						</Transition>
+						<Transition duration={500} scale={125}>
+							<SCSSLogo width={50} height={50} />
+						</Transition>
+						<Transition duration={500} scale={125}>
+							<TailwindCSSLogo width={50} height={50} />
+						</Transition>
+						<Transition duration={500} scale={125}>
+							<TailwindCSSLogo width={50} height={50} />
+						</Transition>
+						<Transition duration={500} scale={125}>
+							<TailwindCSSLogo width={50} height={50} />
+						</Transition>
+					</div>
+				</Card>
 				<Card className="col-span-1 row-span-1 group">
 					<div className="text-2xl custom-gradient-text">{constants.SHARP_EYE}</div>
 					<p className="custom-bento-text text-md mt-4">{constants.SHARP_EYE_DESC}.</p>
 				</Card>
-				<Card className="col-span-1 row-span-1 group">
-					<div className="text-2xl custom-gradient-text">Team Player</div>
-					<p className="custom-bento-text text-md mt-4">
-						Worked closely with cross-functional teams to design, develop, and deploy applications.
-					</p>
-				</Card>
-				<Card className="col-span-2 row-span-1 md:col-span-3 md:row-span-1 group" width="max-w-xl">
-					<div className="text-2xl custom-gradient-text">Skills</div>
-					<div className="mt-5 flex space-x-7">
-						<Transition duration={200} scale={105}>
-							<HTMLLogo width={60} height={60} />
-						</Transition>
-						<Transition duration={500} scale={125}>
-							<CSSLogo width={60} height={60} />
-						</Transition>
-						<Transition duration={500} scale={125}>
-							<JavaScriptLogo width={60} height={60} />
-						</Transition>
-						<Transition duration={500} scale={125}>
-							<TypeScriptLogo width={60} height={60} />
-						</Transition>
-						<Transition duration={500} scale={125}>
-							<ReactLogo width={60} height={60} />
-						</Transition>
-						<Transition duration={500} scale={125}>
-							<SCSSLogo width={60} height={60} />
-						</Transition>
-					</div>
-					<div className="mt-5 flex space-x-7">
-						<Transition duration={500} scale={125}>
-							<SCSSLogo width={60} height={60} />
-						</Transition>
-						<Transition duration={500} scale={125}>
-							<TailwindCSSLogo width={60} height={60} />
-						</Transition>
-					</div>
-				</Card>
 
-				<Card className="col-span-1 row-span-1 group">
-					<div className="text-2xl custom-gradient-text">Problem Solving</div>
-					<p className="custom-bento-text text-md mt-4">
-						Excel at identifying and resolving complex UI and functionality issues, ensuring optimal
-						UI/UX across various devices and browsers.
-					</p>
-				</Card>
-				<Card className="col-span-1 row-span-1 group">
+				<Card className="col-span-2 row-span-1 group">
 					<div className="text-2xl custom-gradient-text">{constants.TECHNICAL_LEADERSHIP}</div>
 					<p className="custom-bento-text text-md mt-4">
 						{constants.TECHNICAL_LEADERSHIP_DESCRIPTION}
--- src/components/Main/contact.tsx
+++ src/components/Main/contact.tsx
@@ -5,17 +5,17 @@ import Button from "../UI/button";
 
 const Contact = () => {
 	return (
-		<div className="ml-3 mr-3">
+		<div className="mx-5">
 			{/* <div className="text-3xl leading-normal text-slate-50 max-w-md">{constants.PROJECT}</div> */}
 			<div className="text-lg leading-normal text-slate-400 mt-2 max-w-md">
 				{constants.PROJECT_DESCRIPTION}
 			</div>
 			<div>
-				<div className="flex space-x-6">
+				<div className="flex space-x-2">
 					<a href="mailto:rahulteja.dev@gmail.com">
 						<Button label={constants.PROJECT_BUTTON} />
 					</a>
-					{/* <span className="text-slate-50 mt-6 text-4xl">{constants.OR}</span> */}
+					<span className="hidden text-slate-50 mt-6 text-4xl">{constants.OR}</span>
 					<a
 						target="_blank"
 						href="https://calendly.com/rahulteja-dev/30min"
@@ -25,10 +25,10 @@ const Contact = () => {
 					</a>
 				</div>
 				<div className="flex">
-					<div className="flex justify-center mt-6">
+					<div className="flex justify-center mt-6 ml-2">
 						<BadgePing />
 					</div>
-					<div className="text-lg ml-2 mt-3 text-slate-50">
+					<div className="text-lg ml-3 mt-3 text-slate-50">
 						{constants.PROJECT_BADGE_PING}
 						<span className="text-2xl">👨🏻‍💻</span>
 					</div>
